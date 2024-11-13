be47cf2c4f0415a17de1fb2bcdcd9df54aaaff52
Min Qi <neominqi@outlook.com>
2024-11-13 05:17:40 -0500

    update control flow

--- Labs/controlFlow/control_flow.js
+++ Labs/controlFlow/control_flow.js
@@ -48,3 +48,10 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+//
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
